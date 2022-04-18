1932224b865445d7ea7ecdc9d41e5444268121e4
zixiao18 <45200890+zixiao18@users.noreply.github.com>
2022-04-18 09:33:00 +0800

    hw4

---
+++ hw4_HPC/q1.cu
@@ -0,0 +1,101 @@
+// We made modifications based on gpu03.cu shown in class
+// $ nvcc -arch=sm_61 gpu03.cu -o gpu03 -Xcompiler -fopenmp
+// flag -Xcompiler passes next flag directly to compiler
+#include <algorithm>
+#include <stdio.h>
+#include <omp.h>
+#include <string>
+#include <math.h>
+
+#define N 4096 // row size 
+#define M 2048 // column size
+#define BLOCK_SIZE 1024
+
+void mat_mul (double* A, double* V, double *C){
+    //#pragma omp parallel for collapse(2)
+    for (long i = 0; i < N; i++) {
+        double inner_prod = 0;
+        for (long j = 0; j < M; j++) {
+            //#pragma omp atomic update 
+            inner_prod += A[i*M+j] * V[j];
+        }
+        C[i] = inner_prod;
+    }
+}
+
+__global__ 
+void mat_mul_kernel(double* A, double* V, double* C, double* temp) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < N) {
+        double value = 0;
+        for (long i = 0; i < M; i++) {
+            value += A[idx*M+i] * V[i];
+        }
+        C[idx] = value;
+    }
+}
+
+void Check_CUDA_Error(const char *message){
+  cudaError_t error = cudaGetLastError();
+  if(error!=cudaSuccess) {
+    fprintf(stderr,"ERROR: %s: %s\n", message, cudaGetErrorString(error) );
+    exit(-1);
+  }
+}
+
+int main(int argc, char** argcv) {
+    long col_size = M;
+    long row_size = N;
+
+    double* A = (double*) malloc(col_size*row_size * sizeof(double));
+    double* V = (double*) malloc(col_size * sizeof(double));
+    double* C = (double*) malloc(row_size * sizeof(double));
+    double* C_ref = (double*) malloc(row_size * sizeof(double));
+
+    for (long i = 0; i < col_size*row_size; i++) A[i] = drand48();
+    for (long i = 0; i < col_size; i++) V[i] = drand48();
+    for (long i = 0; i < row_size; i++) C[i] = 0.0;
+    for (long i = 0; i < row_size; i++) C_ref[i] = 0.0;
+
+    double tt = omp_get_wtime();
+    mat_mul(A,V,C_ref);
+    printf("CPU %f s\n", omp_get_wtime()-tt);
+    double cpu_bandwidth = 3*M*N*sizeof(double)/(omp_get_wtime()-tt)/1e9;
+    printf("CPU bandwidth = %f GB/s\n", cpu_bandwidth);
+
+    double *A_d, *V_d, *C_d, *temp;
+    cudaMalloc(&A_d, col_size*row_size*sizeof(double));
+    Check_CUDA_Error("malloc x failed");
+    cudaMalloc(&V_d, col_size*sizeof(double));
+    cudaMalloc(&C_d, row_size*sizeof(double));
+    cudaMalloc(&temp, col_size*row_size*sizeof(double));
+
+    tt = omp_get_wtime();
+    cudaMemcpy(A_d, A, col_size*row_size*sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(V_d, V, col_size*sizeof(double), cudaMemcpyHostToDevice);
+    double ttinner = omp_get_wtime();
+    mat_mul_kernel<<<N/BLOCK_SIZE,BLOCK_SIZE>>>(A_d,V_d,C_d,temp);
+    cudaDeviceSynchronize();
+    ttinner = omp_get_wtime() - ttinner;
+    cudaMemcpy(C, C_d, row_size*sizeof(double), cudaMemcpyDeviceToHost);
+    printf("GPU %f s, %f s\n", omp_get_wtime()-tt, ttinner);
+
+    double bandwidth = 3*M*N*sizeof(double)/(omp_get_wtime()-tt)/1e9;
+    printf("GPU bandwidth = %f GB/s\n", bandwidth);
+
+    double err = 0;
+    for (long i = 0; i < N; i++) err += fabs(C[i]-C_ref[i]);
+    printf("Error = %f\n", err);
+
+    cudaFree(A_d);
+    cudaFree(V_d);
+    cudaFree(C_d);
+    cudaFree(temp);
+
+    free(A);
+    free(V);
+    free(C);
+    free(C_ref);
+
+    return 0;
+}
---
+++ hw4_HPC/q2.cu
@@ -0,0 +1,125 @@
+#include <algorithm>
+#include <stdio.h>
+//#include <utils.h>
+#include <stdlib.h>
+#include <math.h>
+#ifdef _OPENMP
+    #include <omp.h>
+#endif
+
+#define BLOCK_SIZE_X 32
+#define BLOCK_SIZE_Y 32
+
+long double calculate_resid(double *u, long* f, double N) {
+    double resid = 0.0;
+    double err = 0.0;
+    double h = 1/(N+1); 
+    int np2 = (int) N + 2;
+    for (long i = 1; i <= N; i++) { 
+        for (long j = 1; j <= N; j++) {
+            err = fabs(f[i*np2+j]-(4*u[i*np2+j]-u[(i-1)*np2+j]-u[i*np2+j-1]-u[(i+1)*np2+j]-u[i*np2+j+1])/h/h);
+            resid = std::max(resid,err);
+        } 
+    }
+    return resid;
+}
+
+void jacobi_cpu(double* unew, double* uold, long* f, int N, double h) {
+    int np2 = N+2;
+    double residual = calculate_resid(unew,f,N);
+
+    for (long k = 0; k < 5000; k++) {
+        #ifdef _OPENMP
+            #pragma omp parallel
+        #endif
+        {
+            #ifdef _OPENMP
+               #pragma omp for
+            #endif
+            for (long i = 0; i < np2*np2; i++) {
+                uold[i] = unew[i];
+            } 
+            #ifdef _OPENMP
+                #pragma omp barrier
+                #pragma omp for collapse(2)
+            #endif  
+            for (long j=1; j < N+1; j+=1) {
+                    for (long i = 1; i < N+1; i+=1) {
+                        unew[i*np2+j]=(uold[(i-1)*np2+j]+uold[i*np2+j-1]+uold[(i+1)*np2+j]+uold[i*np2+j+1]+f[i*np2+j]*h*h)/4.0;
+                    }
+                }
+        }
+        double resid_temp = calculate_resid(unew,f,N);
+        
+    }
+}
+
+__global__
+void jacobi_gpu(double* unew, double* uold, long* f, const int N, const double h){
+    int j = blockIdx.x * blockDim.x + threadIdx.x;
+    int i = blockIdx.y * blockDim.y + threadIdx.y;
+    if (i>0 && i<N-1 && j >0 && j<N-1) {
+        unew[i*N+j] = (uold[(i-1)*N+j] + uold[(i+1)*N+j] + uold[i*N+j+1] + uold[i*N+j-1] +f[i*N+j]*h*h)/4.0;
+    }
+}
+
+int main(int argc, char** argv) {
+    int N = 256;
+    int np2 = (int) N + 2;
+    double* unew = (double*) malloc(np2*np2*sizeof(double));
+    double* uold = (double*) malloc(np2*np2*sizeof(double));
+    double* utemp = (double*) malloc(np2*np2*sizeof(double));
+    long* f = (long*) malloc(np2*np2*sizeof(long));
+    double n = (double) N;
+    double h = 1/(n+1);
+   
+
+    // initialization
+    for (long i = 0; i < np2*np2; i++) unew[i] = 0;
+    for (long i = 0; i < np2*np2; i++) f[i] = 1.0;
+    double residual = calculate_resid(unew,f,N);
+
+    double *unew_d, *uold_d;
+    long* f_d;
+    cudaMalloc(&unew_d, np2*np2*sizeof(double));
+    cudaMalloc(&uold_d, np2*np2*sizeof(double));
+    cudaMalloc(&f_d, np2*np2*sizeof(long));
+
+    double tt = omp_get_wtime();
+    cudaMemcpy(unew_d, unew, np2*np2*sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(f_d, f, np2*np2*sizeof(long), cudaMemcpyHostToDevice);
+    double copy_t = omp_get_wtime()-tt;
+
+    tt = omp_get_wtime();
+    jacobi_cpu(unew,uold,f,N,h);
+    printf("CPU %f s\n", omp_get_wtime()-tt);
+
+    dim3 block(BLOCK_SIZE_X,BLOCK_SIZE_Y);
+    dim3 grid(np2/BLOCK_SIZE_X, np2/BLOCK_SIZE_Y);
+
+    tt = omp_get_wtime();
+    for (int k=0; k<5000; k+=2) {
+        jacobi_gpu<<<grid,block>>>(uold_d,unew_d,f_d,N+2,h);
+        cudaDeviceSynchronize();
+        jacobi_gpu<<<grid,block>>>(unew_d,uold_d, f_d,N+2,h);
+        cudaDeviceSynchronize();
+    }
+    cudaMemcpy(utemp, unew_d, np2*np2*sizeof(double), cudaMemcpyDeviceToHost);
+    tt = omp_get_wtime()-tt;
+    printf("GPU %f s, %f s\n", tt+copy_t, tt);
+
+    double error = 0;
+    for (int i=0; i<N; i++) error += (utemp[i] - unew[i]);
+
+    printf("Error: %f \n", error);
+   
+    free(unew);
+    free(uold);
+    free(utemp);
+    free(f);
+    cudaFree(unew_d);
+    cudaFree(uold_d);
+    cudaFree(f_d);
+    return 0;
+
+}
